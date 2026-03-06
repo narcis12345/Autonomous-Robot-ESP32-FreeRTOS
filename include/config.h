6181b2ab46a4b55da29d7b75d15cf660d7adc32a
Negoita Andrei-Narcis <166415471+narcis12345@users.noreply.github.com>
2026-03-06 18:37:45 +0200

    config.h

--- include/config.h
+++ include/config.h
@@ -1,7 +1,10 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-#include "driver/gpio.h"
+#include <driver/gpio.h>
+
+//Constante
+extern const float OFFSET_BRAT_MM;
 
 //Pini motoare
 #define MOTOR_A_INAPOI GPIO_NUM_26 
@@ -18,27 +21,28 @@
 #define I2C_MASTER_FREQ_HZ 100000
 #define PCA9685_ADDR 0x40
 #define SERVO_CANAL_SCANARE 0
-#define SERVO_CANAL_STABILITATE 3
+//#define SERVO_CANAL_STABILITATE 8
+//#define SERVO_CANAL_VERTICAL 4
 
 //Pin detector + constante
 #define PIN_SEMNAL_METAL GPIO_NUM_4
 #define NUMAR_CITIRI_PENTRU_CONFIRMARE 10
-#define PRAG_DETECTII_POZITIVE_NECESARE 7
+#define PRAG_DETECTII_POZITIVE_NECESARE 5
 
 //Constante pentru unghiuri servo
 #define UNGHI_CENTRU 120
 #define UNGHI_STANGA (UNGHI_CENTRU + 90)  
 #define UNGHI_DREAPTA (UNGHI_CENTRU - 90)
-#define UNGHI_STABILITATE 60 
+//#define UNGHI_STABILITATE 80
+//#define UNGHI_VERTICAL 40
 
 //Constante delay-uri
-#define DELAY_VIRAJ_MS 750
-#define DELAY_MERS_LATERAL_MS 1000
+#define DELAY_VIRAJ_MS 700
+#define DELAY_MERS_LATERAL_MS 800
 #define DELAY_STOP_MS 300
 #define DELAY_INTRE_CITIRI_MS 5
-#define DURATA_AVANSARE_NORMAL_MS 1000
-#define DELAY_PAS_SERVO_SCANARE_MS 30
-#define TIMP_DEPLASARE_SIGURANTA_MS 800
+#define DURATA_AVANSARE_NORMAL_MS 600
+#define DELAY_PAS_SERVO_SCANARE_MS 5
 
 //Alte define-uri
 #define BUTON_START GPIO_NUM_0
